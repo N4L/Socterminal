ab312352cb20270005ae46cf239222608aa45a49
NurvX <107164469+akshay-nehate@users.noreply.github.com>
2023-04-30 21:44:50 +0530

    unshorted fixed

--- Sooty.py
+++ Sooty.py
@@ -511,76 +511,25 @@ def urlscanio():
             console.print("[bold red]Invalid response. Please enter 'y' or 'n'.[/bold red]")  
     press_any_key()
 
-
-
-
-
-
-
-    #print("\n --------------------------------- ")
-    #print("\n        U R L S C A N . I O        ")
-    #print("\n --------------------------------- ")
-    #url_to_scan = str(input('\nEnter url: ').strip())
-#
-    #try:
-    #    type_prompt = str(input('\nSet scan visibility to Public? \nType "1" for Public or "2" for Private: '))
-    #    if type_prompt == '1':
-    #        scan_type = 'public'
-    #    else:
-    #        scan_type = 'private'
-    #except:
-    #    print('Please make a selection again.. ')
-#
-    #headers = {
-    #    'Content-Type': 'application/json',
-    #    'API-Key': configvars.data['URLSCAN_IO_KEY'],
-    #}
-#
-    #response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data='{"url": "%s", "%s": "on"}' % (url_to_scan, scan_type)).json()
-#
-    #try:
-    #    if 'successful' in response['message']:
-    #        print('\nNow scanning %s. Check back in around 1 minute.' % url_to_scan)
-    #        uuid_variable = str(response['uuid']) # uuid, this is the factor that identifies the scan
-    #        time.sleep(45) # sleep for 45 seconds. The scan takes awhile, if we try to retrieve the scan too soon, it will return an error.
-    #        scan_results = requests.get('https://urlscan.io/api/v1/result/%s/' % uuid_variable).json() # retrieving the scan using the uuid for this scan
-#
-    #        task_url = scan_results['task']['url']
-    #        verdicts_overall_score = scan_results['verdicts']['overall']['score']
-    #        verdicts_overall_malicious = scan_results['verdicts']['overall']['malicious']
-    #        task_report_URL = scan_results['task']['reportURL']
-#
-    #        print("\nurlscan.io Report:")
-    #        print("\nURL: " + task_url)
-    #        print("\nOverall Verdict: " + str(verdicts_overall_score))
-    #        print("Malicious: " + str(verdicts_overall_malicious))
-    #        print("urlscan.io: " + str(scan_results['verdicts']['urlscan']['score']))
-    #        if scan_results['verdicts']['urlscan']['malicious']:
-    #            print("Malicious: " + str(scan_results['verdicts']['urlscan']['malicious'])) # True
-    #        if scan_results['verdicts']['urlscan']['categories']:
-    #            print("Categories: ")
-    #        for line in scan_results['verdicts']['urlscan']['categories']:
-    #            print("\t"+ str(line)) # phishing
-    #        for line in scan_results['verdicts']['engines']['verdicts']:
-    #            print(str(line['engine']) + " score: " + str(line['score'])) # googlesafebrowsing
-    #            print("Categories: ")
-    #            for item in line['categories']:
-    #                print("\t" + item) # social_engineering
-    #        print("\nSee full report for more details: " + str(task_report_URL))
-    #        print('')
-    #    else:
-    #        print(response['message'])
-    #except:
-    #    print(' Error reaching URLScan.io')
-
 def unshortenUrl():
-    print("\n --------------------------------- ")
-    print("   U R L   U N S H O R T E N E R  ")
-    print(" --------------------------------- ")
+    import requests
+    from rich.console import Console
+    from rich.table import Table
+
+    console = Console()
+    console.rule("\n[bold magenta] U N S H O R T E N - A N Y - U R L S [/]\n")
     link = str(input(' Enter URL: ').strip())
     req = requests.get(str('https://unshorten.me/s/' + link))
-    print(req.text)
 
+    table = Table()
+    table.add_column("Original URL",style="magenta")
+    table.add_column("Unshortened URL", style="green")
+    table.add_row(link, req.text)
+
+    console.print(table)
+    pyperclip.copy(req.text)
+    console.print("Unshortened link copied to clipboard!", style="yellow")
+    press_any_key()
     decoderMenu()
 
 def b64Decoder():
